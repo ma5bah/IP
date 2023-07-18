397734f746e9abff0fbb9664da08c1765aa74d1d
Abu Md. Masbah Uddin <masbahuddin60@gmail.com>
2023-07-18 21:53:34 +0600

    course video page done

--- CourseMaterial/courseMaterial.js
+++ CourseMaterial/courseMaterial.js
@@ -1,29 +1,58 @@
-let current_video_index=localStorage.getItem("current_video_index");
-let current_video=document.getElementById("current_video");
-let list_of_videos=document.getElementById("list_of_videos");
+let current_video_index = localStorage.getItem("current_video_index");
+let current_video = document.getElementById("current_video");
+let list_of_videos = document.getElementById("list_of_videos");
+let youtube_video_data;
 
+on_course_page_load = async () => {
+    const queryString = window.location.search;
+    console.log(queryString);
+    const urlParams = new URLSearchParams(queryString);
+    const course_id = urlParams.get('course_id')
+    let url=`http://localhost:3000/course/get_course?course_id=${course_id}`;
 
-on_course_page_load=async ()=>{
-
-const res=    await fetch("http://localhost:3000/course/get_course",{
-        method:"GET",
-        headers:{
-            "Content-Type":"application/json"
+    const res = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
         },
-        // body:JSON.stringify({
-        //     course_id:localStorage.getItem("course_id")
-        // })    
     })
-    const data=await res.json();
-    console.log(data);
-    if(!current_video_index){
-        current_video_index=0;
-        localStorage.setItem('current_video_index',current_video_index)
+    youtube_video_data = await res.json();
+    if (!current_video_index) {
+        current_video_index = 0;
+        localStorage.setItem('current_video_index', current_video_index)
     }
-    let video_url = data['items'][current_video_index]['snippet']['resourceId']['videoId'];
-    // console.log(video_url)
-    const docu = `<iframe width="100%" height="100%"
-        src="https://www.youtube.com/embed/${video_url}">
-    </iframe>`
-    current_video.innerHTML=docu;
+    
+    let video_url = youtube_video_data['items'][current_video_index]['snippet']['resourceId']['videoId'];
+    setVideo(video_url)
+    setListOfVideo(youtube_video_data);
+}
+onListVideoClick = (idx) => {
+    localStorage.setItem('current_video_index', idx)
+    setVideo(youtube_video_data['items'][idx]['snippet']['resourceId']['videoId'])
+}
+setVideo = (video_url) => {
+    current_video.innerHTML = `<iframe width="100%" height="100%"
+    src="https://www.youtube.com/embed/${video_url}">
+    </iframe>`;
+}
+setListOfVideo = (video_data) => {
+    console.log(video_data);
+    let list_of_videos_html_data = "";
+    for (let index = 0; index < video_data['items'].length; index++) {
+        const temp_video_data = video_data['items'][index];
+        
+        const video_title= temp_video_data['snippet']['title'];
+        if(video_title==="Private video"){
+        continue;
+        }
+        const default_thumbnail_url= temp_video_data['snippet']['thumbnails']['default']['url'];
+        
+        list_of_videos_html_data = list_of_videos_html_data + `<div id="video_${index}" onclick="onListVideoClick(${index})"  class = "videos">
+        <div class="video_thumb">
+            <img src='${default_thumbnail_url}' width="100%" height="100%">
+        </div>
+        <div class="video_title">${video_title}</div>
+        </div>`
+    }
+    list_of_videos.innerHTML = list_of_videos_html_data;
 }
